8a04d4ad7dbb960e118f8bd6cafb91b58a4a7d81
franhouses <francasasmartinez@gmail.com>
2023-03-02 20:29:41 +0100

    refactorización realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -11,21 +11,90 @@ package Hogar;
  */
 public class Vivienda {
     
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+    private double precio;
+    private int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
+    
+    public Vivienda(double precio, int numHabitaciones, double superficie,boolean parking, String estado, String propietario)
+    {
+        this.precio = precio;
+        this.numHabitaciones = numHabitaciones;
+        this.superficie = superficie;
+        this.parking = parking;
+        this.estado = estado;
+        this.propietario = propietario;
+    }
+    
+    
     
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
-    public void actualizarPrecio()
+    public void actualizarPrecio(double descuento)
     {
-        precio = precio - precio * 0.05;
+        precio = precio - precio * descuento;
     }
     
+    public void imprimirVivienda()
+    {
+        System.out.println("Precio: " + this.precio);
+        System.out.println("Número de habitaciones: "+ this.numHabitaciones);
+        System.out.println("Superficie en m2: " + this.superficie);
+        System.out.println("¿Dispone de parking?: " + this.parking);
+        System.out.println("Estado: "+ this.estado);
+        System.out.println("Propietario: "+ this.propietario);
+    }
+    
+    public double getPrecio(){
+        return precio;
+    }
+    
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    public int getNumHabitaciones(){
+        return numHabitaciones;
+    }
+    
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+    
+    public double getSuperficie(){
+        return superficie;
+    }
+    
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    
+    public boolean getParking(){
+        return parking;
+    }
+    
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }   
+
+    public String getEstado(){
+        return estado;
+    }
+    
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }  
+
+    public String getPropietario(){
+        return propietario;
+    }
+    
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }           
 }
