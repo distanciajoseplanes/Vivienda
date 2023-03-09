e4afb8b9debbea8760abbcacbce6b947310441c5
EvaMolpeceresAlburquerque (758553@alu.murciaeduca.es) <>
2023-03-09 19:04:22 +0100

    documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -10,6 +10,10 @@ package Hogar;
  * @author paqui
  */
 public class Vivienda {
+    
+    /**La clase Vivienda replica las características de una vivienda real, y con 
+     * sus métodos nos permitirá manejar información relativa a una vivienda.
+     */
 
     double precio;
     int numHabitaciones;
@@ -18,11 +22,24 @@ public class Vivienda {
     String estado;
     String propietario;
     
+    /**
+     *Método constructor. Acepta:
+     * @param precio
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
 
+    /**
+     *Método constructor. Acepta:
+     * @param precio
+     * @param numHabitaciones
+     * @param superficie
+     * @param parking
+     * @param estado
+     * @param propietario
+     */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -35,6 +52,7 @@ public class Vivienda {
     
     
     /**
+     * Método getter para acceder a los atributos privados.
      * @return the precio
      */
     public double getPrecio() {
@@ -42,6 +60,7 @@ public class Vivienda {
     }
 
     /**
+     * Método setter para modificar los atributos privados.
      * @param precio the precio to set
      */
     public void setPrecio(double precio) {
@@ -49,6 +68,7 @@ public class Vivienda {
     }
 
     /**
+     * Método getter para acceder a los atributos privados.
      * @return the numHabitaciones
      */
     public int getNumHabitaciones() {
@@ -56,6 +76,7 @@ public class Vivienda {
     }
 
     /**
+     * Método setter para modificar los atributos privados.
      * @param numHabitaciones the numHabitaciones to set
      */
     public void setNumHabitaciones(int numHabitaciones) {
@@ -63,6 +84,7 @@ public class Vivienda {
     }
 
     /**
+     * Método getter para acceder a los atributos privados.
      * @return the superficie
      */
     public double getSuperficie() {
@@ -70,6 +92,7 @@ public class Vivienda {
     }
 
     /**
+     * Método setter para modificar los atributos privados.
      * @param superficie the superficie to set
      */
     public void setSuperficie(double superficie) {
@@ -77,6 +100,7 @@ public class Vivienda {
     }
 
     /**
+     * Método equals para comprobar si la vivienda tiene parking o no.
      * @return the parking
      */
     public boolean isParking() {
@@ -84,6 +108,7 @@ public class Vivienda {
     }
 
     /**
+     * Método setter para modificar los atributos privados.
      * @param parking the parking to set
      */
     public void setParking(boolean parking) {
@@ -91,6 +116,7 @@ public class Vivienda {
     }
 
     /**
+     * Método getter para acceder a los atributos privados.
      * @return the estado
      */
     public String getEstado() {
@@ -98,6 +124,7 @@ public class Vivienda {
     }
 
     /**
+     * Método setter para modificar los atributos privados.
      * @param estado the estado to set
      */
     public void setEstado(String estado) {
@@ -105,6 +132,7 @@ public class Vivienda {
     }
 
     /**
+     * Método getter para acceder a los atributos privados.
      * @return the propietario
      */
     public String getPropietario() {
@@ -112,15 +140,18 @@ public class Vivienda {
     }
 
     /**
+     * Método setter para modificar los atributos privados.
      * @param propietario the propietario to set
      */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
    
-    
-    
-    
+    /**
+     * Método que facilita la actualización del precio de acuerdo al descuento
+     * introducido.
+     * @param descuento
+     */
     public void actualizarPrecio(double descuento)
     {
         setPrecio(getPrecio() - getPrecio() * descuento);
