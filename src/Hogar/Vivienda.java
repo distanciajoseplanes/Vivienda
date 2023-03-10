865b2ddd32c31dee530b467e659c4d7eb8081ccb
miry_ <miry_@miriam>
2023-03-10 20:37:31 +0100

    Subir los archivos a GitHub

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -6,11 +6,14 @@
 package Hogar;
 
 /**
- *
+ * Esta clase muestra los parámetros de una vivienda
+ * @author Miriam Cegarra Garcia
  * @author paqui
+ * @version 13/13/2023/A
  */
-public class Vivienda {
-    
+public class Vivienda 
+{
+    //Atributos
     private double precio;
     private int numHabitaciones;
     private double superficie;
@@ -18,6 +21,16 @@ public class Vivienda {
     private String estado;
     private String propietario;
 
+    /**
+     * Constructor para la clase vivienda
+     * @param precio El parámetro precio define el valor de la vivienda en euros
+     * @param numHabitaciones El parámetro numHabitaciones define el número de habitaciones de la vivienda
+     * @param superficie El parámetro superficie define la superficie en metros cuadrados de la vivienda
+     * @param parking El parámetro parking define si la vivienda dispone de parking o no 
+     * @param estado El parámetro estado define el estado actual de la vivienda
+     * @param propietario El parámetro propietario define el nombre del propietario de la vivienda
+     */
+    
     public Vivienda(double precio, int numHabitaciones, double superficie, 
                     boolean parking, String estado, String propietario) 
     {
@@ -28,8 +41,18 @@ public class Vivienda {
         this.estado = estado;
         this.propietario = propietario;
     }
-
     
+
+    /**
+     * Constructor que solo permite introducir el precio
+     * @param precio El parámetro precio define el valor de la vivienda en euros
+     */
+    public Vivienda (double precio)
+    {
+        this.precio = precio;
+    }
+    
+    //Métodos getter y setter   
     public double getPrecio() {
         return precio;
     }
@@ -78,26 +101,27 @@ public class Vivienda {
         this.propietario = propietario;
     }
     
-    
-    
-    public Vivienda (double precio)
-    {
-        this.precio = precio;
-    }
-    
+    /**
+     * Método que calcula el precio final de la vivienda restándole un descuento
+     * @param descuento El parámetro descuento es el porcentaje que se desea restar al precio total de la vivienda
+     */
     public void actualizarPrecio(int descuento)
     {
         precio = precio - precio * descuento/100;
     }
     
-    public void imprimirVivienda(Vivienda casa1)
+    /**
+     * Método que imprime por pantalla los datos de la vivienda
+     * @param casa El parámetro casa representa los datos de la vivienda
+     */
+    public void imprimirVivienda(Vivienda casa)
     {
-        System.out.println("Precio: " + casa1.precio);
-        System.out.println("Número de habitaciones: "+ casa1.numHabitaciones);
-        System.out.println("Superficie en m2: " + casa1.superficie);
-        System.out.println("¿Dispone de parking?: " + casa1.parking);
-        System.out.println("Estado: "+ casa1.estado);
-        System.out.println("Propietario: "+ casa1.propietario);
+        System.out.println("Precio: " + casa.precio);
+        System.out.println("Número de habitaciones: "+ casa.numHabitaciones);
+        System.out.println("Superficie en m2: " + casa.superficie);
+        System.out.println("¿Dispone de parking?: " + casa.parking);
+        System.out.println("Estado: "+ casa.estado);
+        System.out.println("Propietario: "+ casa.propietario);
     }
     
 }
