2ec991cb642b7164336814609b016bea08e119b6
miry_ <miry_@miriam>
2023-03-10 19:27:24 +0100

    Refactorización realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -11,21 +11,93 @@ package Hogar;
  */
 public class Vivienda {
     
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+    private double precio;
+    private int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
+
+    public Vivienda(double precio, int numHabitaciones, double superficie, 
+                    boolean parking, String estado, String propietario) 
+    {
+        this.precio = precio;
+        this.numHabitaciones = numHabitaciones;
+        this.superficie = superficie;
+        this.parking = parking;
+        this.estado = estado;
+        this.propietario = propietario;
+    }
+
+    
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public boolean isParking() {
+        return parking;
+    }
+
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+    
+    
     
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
-    public void actualizarPrecio()
+    public void actualizarPrecio(int descuento)
     {
-        precio = precio - precio * 0.05;
+        precio = precio - precio * descuento/100;
+    }
+    
+    public void imprimirVivienda(Vivienda casa1)
+    {
+        System.out.println("Precio: " + casa1.precio);
+        System.out.println("Número de habitaciones: "+ casa1.numHabitaciones);
+        System.out.println("Superficie en m2: " + casa1.superficie);
+        System.out.println("¿Dispone de parking?: " + casa1.parking);
+        System.out.println("Estado: "+ casa1.estado);
+        System.out.println("Propietario: "+ casa1.propietario);
     }
     
 }
