75dc8ca97de8405bac7b6481fce6687659b38100
Marisol <Marisol@192.168.1.110>
2023-03-11 19:41:45 +0100

    refactorización finalizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -9,7 +9,7 @@ package Hogar;
  *
  * @author paqui
  */
-public class Vivienda {
+public final class Vivienda {
     
     double precio;
     int numHabitaciones;
@@ -17,15 +17,60 @@ public class Vivienda {
     boolean parking;
     String estado;
     String propietario;
-    
-    public Vivienda (double precio)
-    {
-        this.precio = precio;
+
+public void setPrecio(double precio) {
+    this.precio = precio;}
+
+public void setNumHabitaciones(int numHabitaciones) {
+    this.numHabitaciones = numHabitaciones;}
+
+public void setSuperficie(double superficie) {
+    this.superficie = superficie;}
+
+public void setParking(boolean parking) {
+    this.parking = parking;}
+
+public void setEstado(String estado) {
+    this.estado = estado;}
+
+public void setPropietario(String propietario) {
+    this.propietario = propietario;}
+   
+
+public double getPrecio() {
+    return precio;}
+
+public int getNumHabitaciones() {
+    return numHabitaciones;}
+
+public double getSuperficie() {
+    return superficie;}
+
+public boolean isParking() {
+    return parking;}
+
+public String getEstado() {
+    return estado;}
+
+public String getPropietario() {
+    return propietario;}  
+
+public Vivienda(int numHabitaciones) {
+    this.setPrecio(precio);
+    this.setNumHabitaciones(numHabitaciones);
+    this.setSuperficie(superficie);
+    this.setParking(parking);
+    this.setEstado(estado);
+    this.setPropietario(propietario);
+}
+
+
+    Vivienda() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
-    public void actualizarPrecio()
-    {
-        precio = precio - precio * 0.05;
+    public void actualizarPrecio(double descuento){
+       this.precio = this.precio - (this.precio * descuento / 100);
     }
     
 }
