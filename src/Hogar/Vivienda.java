54c0d6795395d0cca92cab339906a76916201ab8
mari_ <mari_@DESKTOP-U6JM85B>
2023-03-12 05:03:14 +0100

    documentacion realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -9,15 +9,47 @@ package Hogar;
  *
  * @author paqui
  */
+/**
+ *Con la clase Vivienda se hace una descripción de las características
+ *de una vivienda, incluyendo entre sus atributos el precio, número de
+ * habitaciones, superficie, si tiene plaza de aparcamiento, el estado en
+ * que se encuentra y el nombre del propietario.
+ */
 public class Vivienda {
-    
+    /**
+     * Precio de la vivienda
+     */
     private double precio;
+    /**
+     * Número de habitaciones
+     */
     private int numHabitaciones;
+    /**
+     * Superficie habitable
+     */
     private double superficie;
+    /**
+     * Disponibilidad de plaza de aparcamiento
+     */
     private boolean parking;
+    /**
+     * Estado en el que se encuentra la vivienda
+     */
     private String estado;
+    /**
+     * Nombre del propietario
+     */
     private String propietario;
-
+    
+    /**
+     * Constructor con todos los atributos de la clase
+     * @param precio
+     * @param numHabitaciones
+     * @param superficie
+     * @param parking
+     * @param estado
+     * @param propietario 
+     */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -27,17 +59,26 @@ public class Vivienda {
         this.propietario = propietario;
     }
     
+    /**
+     * Constructor que solo recibe como parámetro el atributo precio
+     * @param precio 
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
+    /**
+     * Método que aplica un descuento al precio inicial
+     * @param descuento 
+     */
     public void actualizarPrecio(float descuento)
     {
         setPrecio(getPrecio() - descuento);
     }
 
     /**
+     * Método get del atributo precio
      * @return the precio
      */
     public double getPrecio() {
@@ -45,6 +86,7 @@ public class Vivienda {
     }
 
     /**
+     * Método get del atributo numHabitaciones
      * @return the numHabitaciones
      */
     public int getNumHabitaciones() {
@@ -52,6 +94,7 @@ public class Vivienda {
     }
 
     /**
+     * Método get del atributo superficie
      * @return the superficie
      */
     public double getSuperficie() {
@@ -59,6 +102,7 @@ public class Vivienda {
     }
 
     /**
+     * Método booleano que devuelve true si la vivienda tiene parking y false si no la tiene
      * @return the parking
      */
     public boolean isParking() {
@@ -66,6 +110,7 @@ public class Vivienda {
     }
 
     /**
+     * Método get del atributo estado
      * @return the estado
      */
     public String getEstado() {
@@ -73,6 +118,7 @@ public class Vivienda {
     }
 
     /**
+     * Método get del atributo propietario
      * @return the propietario
      */
     public String getPropietario() {
@@ -80,6 +126,7 @@ public class Vivienda {
     }
 
     /**
+     * Método set para cambiar el precio
      * @param precio the precio to set
      */
     public void setPrecio(double precio) {
@@ -87,6 +134,7 @@ public class Vivienda {
     }
 
     /**
+     * Método set para cambiar el número de habitaciones
      * @param numHabitaciones the numHabitaciones to set
      */
     public void setNumHabitaciones(int numHabitaciones) {
@@ -94,6 +142,7 @@ public class Vivienda {
     }
 
     /**
+     * Método set para cambiar la superficie
      * @param superficie the superficie to set
      */
     public void setSuperficie(double superficie) {
@@ -101,6 +150,7 @@ public class Vivienda {
     }
 
     /**
+     * Método set para cambiar el atributo parking
      * @param parking the parking to set
      */
     public void setParking(boolean parking) {
@@ -108,6 +158,7 @@ public class Vivienda {
     }
 
     /**
+     * Método set para cambiar el estado de la vivienda
      * @param estado the estado to set
      */
     public void setEstado(String estado) {
@@ -115,6 +166,7 @@ public class Vivienda {
     }
 
     /**
+     * Método set para cambiar el propietario
      * @param propietario the propietario to set
      */
     public void setPropietario(String propietario) {
