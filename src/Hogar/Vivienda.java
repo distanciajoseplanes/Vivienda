6273248b22c41a1d1d5dfd2ff556add12ee47bbe
josez <josez@LAPTOP-FH2MIHE3.technicolor.net>
2023-03-14 13:34:55 +0100

    documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -6,7 +6,8 @@
 package Hogar;
 
 /**
- *
+ * La clase Vivienda abarca las distintas características fundamentales
+ * e información de una vivienda que vamos a poner en venta.
  * @author paqui
  */
 public class Vivienda {
@@ -21,21 +22,24 @@ public class Vivienda {
     }
 
     /**
-     * @return the precio
+     * @return de precio.
+     * Devuelve el precio base de la vivienda puesta en venta.
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     * @param precio the precio to set.
+     * Indica el parámetro del precio base de la vivienda puesta en venta. 
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the numHabitaciones
+     * @return the numHabitaciones.
+     * Devuelve el número total de habitaciones que hay en la vivienda.
      */
     public int getNumHabitaciones() {
         return numHabitaciones;
@@ -43,13 +47,14 @@ public class Vivienda {
 
     /**
      * @param numHabitaciones the numHabitaciones to set
+     * Parámetro del número de habitaciones que hay en la vivienda.
      */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
     /**
-     * @return the superficie
+     * @return the superficie en m2 que dispone la vivienda.
      */
     public double getSuperficie() {
         return superficie;
@@ -57,6 +62,7 @@ public class Vivienda {
 
     /**
      * @param superficie the superficie to set
+     * Establece los m2 de la vivienda.
      */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
@@ -64,6 +70,7 @@ public class Vivienda {
 
     /**
      * @return the parking
+     * Devuelve la posibilidad de que la vivienda disponga de parking o no.
      */
     public boolean isParking() {
         return parking;
@@ -71,6 +78,7 @@ public class Vivienda {
 
     /**
      * @param parking the parking to set
+     * Establece la posibilidad de que la vivienda disponga de parking o no.
      */
     public void setParking(boolean parking) {
         this.parking = parking;
@@ -78,6 +86,7 @@ public class Vivienda {
 
     /**
      * @return the estado
+     * Devuelve el estado general en el que se encuentra la vivienda.
      */
     public String getEstado() {
         return estado;
@@ -85,6 +94,7 @@ public class Vivienda {
 
     /**
      * @param estado the estado to set
+     * Dicta el estado general en el que se encuentra la vivienda.
      */
     public void setEstado(String estado) {
         this.estado = estado;
@@ -92,6 +102,7 @@ public class Vivienda {
 
     /**
      * @return the propietario
+     * Devuelve el propietario de la vivienda puesta en venta.
      */
     public String getPropietario() {
         return propietario;
@@ -99,6 +110,7 @@ public class Vivienda {
 
     /**
      * @param propietario the propietario to set
+     * Establece el propietario de la vivienda.
      */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
@@ -116,11 +128,22 @@ public class Vivienda {
         this.precio = precio;
     }
     
+    /**
+     * Método para actualizar el precio.
+     * Con el fin de aplicar al precio base un descuento.
+     * @author Jose Antonio Zomeño Pardo
+     */
     public void actualizarPrecio()
     {
         descuento();
     }
-
+    /**
+     * Método para establecer el precio final.
+     * Se multiplica el precio base por 0.05 para obtener la cantidad a restar
+     * a este precio base. El resultado de la resta será el precio final de la
+     * vivienda, obteniendo así un descuento del 5%.
+     * @author Jose Antonio Zomeño Pardo
+     */
     public void descuento() {
         setPrecio(getPrecio() - getPrecio() * 0.05);
     }
