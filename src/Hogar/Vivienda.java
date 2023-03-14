23902b7145230a70fdb96f3cf1516cd9bd13c05c
Valverde <Valverde@DESKTOP-QKQ3MS9>
2023-03-14 18:22:49 +0100

    refactorización realizada documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -7,94 +7,83 @@ package Hogar;
 
 /**
  *
- * @author paqui
+ * @author Daniel Valverde :D
  */
 public class Vivienda {
 
-    /**
-     * @return the precio
+    /** Devuelve el precio de la vivienda
      */
     public double getPrecio() {
         return precio;
     }
 
-    /**
-     * @param precio the precio to set
+    /** Aquí se establece el precio de la vivienda
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
-    /**
-     * @return the numHabitaciones
+    /** Devulve el número de habitaciones que tiene la vivienda
      */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
 
-    /**
-     * @param numHabitaciones the numHabitaciones to set
+    /** Para establecer el número de habitaciones
      */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
-    /**
-     * @return the superficie
+    /** Devuelve la superficie que tiene la vivienda
      */
     public double getSuperficie() {
         return superficie;
     }
 
-    /**
-     * @param superficie the superficie to set
+    /** Se establece la superficie
      */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
-    /**
-     * @return the parking
+    /** Devuelve ''true'' si la vivienda tiene parking, ''false'' si no tiene.
      */
     public boolean isParking() {
         return parking;
     }
 
-    /**
-     * @param parking the parking to set
+    /** Aquí se establece si tiene o no parking
      */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
-    /**
-     * @return the estado
+    /** Devuelve el estado de la vivienda
      */
     public String getEstado() {
         return estado;
     }
 
-    /**
-     * @param estado the estado to set
+    /** Establece el estado de la vivienda
      */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
-    /**
-     * @return the propietario
+    /** Devuelve el nombre del propietario
      */
     public String getPropietario() {
         return propietario;
     }
 
-    /**
-     * @param propietario the propietario to set
+    /** Establece el nombre del propietario
      */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
-
+    /**  Crea una nueva instancia de la clase Vivienda con los valores de atributo especificados
+     */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -103,20 +92,29 @@ public class Vivienda {
         this.estado = estado;
         this.propietario = propietario;
     }
-    
+    /** El precio que tiene la vivienda */
     private double precio;
+    /** EL número de habitaciones de la vivienda */
     private int numHabitaciones;
+    /** La superficie de la vivienda */
     private double superficie;
+    /** Si la vivienda tiene parking o no */
     private boolean parking;
+    /** Establece el estado de esta*/
     private String estado;
+    /** Quién es el propietario de la vivienda */
     private String propietario;
     
+    
+    /** Crea una nueva instancia de la clase Vivienda con un precio especificado
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
-    
+    /** Aplica un descuento al precio actual de la vivienda y actualiza el precio a un nuevo valor con setPrecio
+     */
     public void actualizarPrecio(double descuento) 
     {
     double precioDescuento = getPrecio() - (getPrecio() * (descuento / 100));
