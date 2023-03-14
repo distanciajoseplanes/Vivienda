0ee0bb18af7bbb7b969b0e68d6d508628e668f09
alex_ <alex_@Torre>
2023-03-14 03:41:24 +0100

    refactorizacion realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -11,21 +11,63 @@ package Hogar;
  */
 public class Vivienda {
     
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+    private double precio;
+    private int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
     
-    public Vivienda (double precio)
-    {
+    public Vivienda (double precio,int numHabitaciones,double superficie
+    ,boolean parking,String estado,String propietario){
         this.precio = precio;
+        this.numHabitaciones = numHabitaciones;
+        this.superficie = superficie;
+        this.parking = parking;
+        this.estado= estado;
+        this.propietario = propietario;
     }
     
-    public void actualizarPrecio()
+    public void actualizarPrecio(double descuento)
     {
-        precio = precio - precio * 0.05;
+        precio = precio - precio * (descuento/100);
     }
+    public void setprecio(double precio){
+        this.precio=precio;
+    }
+    public double getprecio(){
+        return precio;
+    }
+    public void setnumHabitaciones(int numHabitaciones){
+        this.numHabitaciones=numHabitaciones;
+    }
+    public int getnumHabitaciones(){
+        return numHabitaciones;
+    }
+    public void setsuperficie(double superficie){
+        this.superficie=superficie;
+    }
+    public double getsuperficie(){
+        return superficie;
+    }
+    public void setparking(boolean parking){
+        this.parking=parking;
+    }
+    public boolean getparking(){
+        return parking;
+    }
+    public void setestado(String estado){
+        this.estado=estado;
+    }
+    public String getestado(){
+        return estado;
+    }
+    public void setpropietario(String propietario){
+        this.propietario=propietario;
+    }
+    public String getpropietario(){
+        return propietario;
+    }
+    
     
 }
