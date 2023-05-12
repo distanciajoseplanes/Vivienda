444b37130232a174e15f478d6c5d45bf6d2f36d0
Juan <12505488Juan>
2023-05-12 11:55:04 +0200

    refactorización realizada.

--- src/Hogar/Hogar.java
+++ src/Hogar/Hogar.java
@@ -11,25 +11,19 @@ package Hogar;
  */
 public class Hogar {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Vivienda v = new Vivienda(120000);
+        Vivienda casa1 = new Vivienda(120000, 3, 150.5, true, "Buen estado", "Juan");
         
-        double precio;
-        int numHabitaciones;
-        double superficie;
-        boolean parking;
-        String estado;
-        String propietario;
+        System.out.println("Datos de la vivienda:");
+        System.out.println("Precio: " + casa1.getPrecio());
+        System.out.println("Número de habitaciones: " + casa1.getNumHabitaciones());
+        System.out.println("Superficie en m2: " + casa1.getSuperficie());
+        System.out.println("¿Dispone de parking?: " + casa1.tieneParking());
+        System.out.println("Estado: " + casa1.getEstado());
+        System.out.println("Propietario: " + casa1.getPropietario());
         
-        System.out.println("Precio: " + v.precio);
-        System.out.println("Número de habitaciones: "+ v.numHabitaciones);
-        System.out.println("Superficie en m2: " + v.superficie);
-        System.out.println("¿Dispone de parking?: " + v.parking);
-        System.out.println("Estado: "+ v.estado);
-        System.out.println("Propietario: "+ v.propietario);
+        casa1.actualizarPrecio(10); // Aplica un 10% de descuento
+        System.out.println("Precio actualizado: " + casa1.getPrecio());
     }
-    
 }
+
