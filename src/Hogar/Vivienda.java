065ac40d78864541d2a02e13cbcd67188a070ad6
LunethIV <immanuesteban@gmail.com>
2024-02-12 16:13:56 +0100

    refactorización realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -11,12 +11,12 @@ package Hogar;
  */
 public class Vivienda {
     
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+    private double precio;
+    private int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
     
     public Vivienda (double precio)
     {
@@ -28,4 +28,61 @@ public class Vivienda {
         precio = precio - precio * 0.05;
     }
     
+    public double getPrecio(){
+        return this.precio;
+    }
+    
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public boolean isParking() {
+        return parking;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+    
+    // Constructor
+    public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario){
+        this.precio = precio;
+        this.numHabitaciones = numHabitaciones;
+        this.superficie = superficie;
+        this.parking = parking;
+        this.estado = estado;
+        this.propietario = propietario;
+    }
 }
