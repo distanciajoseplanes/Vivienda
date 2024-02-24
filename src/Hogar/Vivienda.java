1edcac99327009af66166f244fa0069c4c3132c9
bryancontreras10 <5585701@alu.murciaeduca.es>
2024-02-24 02:00:53 +0100

    Refactorización realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -11,21 +11,75 @@ package Hogar;
  */
 public class Vivienda {
     
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+    private double precio;
+    private int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
     
-    public Vivienda (double precio)
+    public Vivienda (double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario)
     {
+        this.precio = precio;
+        this.numHabitaciones = numHabitaciones;
+        this.superficie = superficie;
+        this.parking = parking;
+        this.estado = estado;
+        this.propietario = propietario;
+    }
+    
+    
+    public void actualizarPrecio(double descuento)
+    {
+        precio = precio - precio * (descuento / 100);
+    }
+    
+    public double getPrecio() {
+        return precio;
+    }
+    
+    public void setPrecio(double precio){
         this.precio = precio;
     }
     
-    public void actualizarPrecio()
-    {
-        precio = precio - precio * 0.05;
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+    
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+    
+    public double getSuperficie(){
+        return superficie;
+    }
+    
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    
+    public boolean isParking() {
+        return parking;
+    }
+    
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+    
+    public String getEstado() {
+        return estado;
+    }
+    
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public String getPropietario() {
+        return propietario;
+    }
+    
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
     }
     
 }
