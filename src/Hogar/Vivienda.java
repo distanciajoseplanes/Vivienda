431b70dc075f1ffbfd0c97686e4e5702386d81fb
Inmaculada-Navarro-Carceles <161589175+Inmaculada-Navarro-Carceles@users.noreply.github.com>
2024-02-28 18:44:55 +0100

    documentación realizada

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -12,7 +12,8 @@ package Hogar;
 public class Vivienda {
 
     /**
-     * @return the precio
+     * Devuelve el precio de la vivienda
+     * @return 
      */
     public double getPrecio() {
         return precio;
@@ -31,77 +32,88 @@ public class Vivienda {
     
 
     /**
-     * @param precio the precio to set
+     * Establece el precio de la vivienda
+     * @param precio 
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the numHabitaciones
+     * Devuelve el número de habitaciones
+     * @return 
      */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
 
     /**
-     * @param numHabitaciones the numHabitaciones to set
+     * Establece el número de habitaciones
+     * @param numHabitaciones 
      */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
     /**
-     * @return the superficie
+     * Devuelve los metros de superficie que tiene la vivienda
+     * @return 
      */
     public double getSuperficie() {
         return superficie;
     }
 
     /**
-     * @param superficie the superficie to set
+     * Establece los metros de superficie que tiene la vivienda
+     * @param superficie 
      */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
     /**
-     * @return the parking
+     * Devuelve verdadero o falso si tiene parking o no la vivienda
+     * @return 
      */
     public boolean isParking() {
         return parking;
     }
 
     /**
-     * @param parking the parking to set
+     * Establece si dispone de parking la vivienda
+     * @param parking 
      */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
     /**
-     * @return the estado
+     * Devuelve el estado en el que está la vivienda
+     * @return 
      */
     public String getEstado() {
         return estado;
     }
 
     /**
-     * @param estado the estado to set
+     * Establece el estado en el que está la vivienda
+     * @param estado 
      */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
     /**
-     * @return the propietario
+     * Devuelve el nombre del propietario
+     * @return 
      */
     public String getPropietario() {
         return propietario;
     }
 
     /**
-     * @param propietario the propietario to set
+     * Establece el nombre del propietario
+     * @param propietario 
      */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
@@ -115,12 +127,21 @@ public class Vivienda {
     private String propietario;
     
     
+    /** 
+     * 
+     * Establece el precio de la vivienda 
+     * @param precio 
+     */
     
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
+    /**
+     * Devuelve el precio de la vivienda aplicando el descuento 
+     * @param descuento 
+     */
     public void actualizarPrecio(Object descuento)
     {
         setPrecio(getPrecio() - getPrecio() * 0.05);
